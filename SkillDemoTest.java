ef470bde4dca63a3ab95f36611b9c2f87a47d503
Eugene Lee <eugenehlee99@gmail.com>
2022-02-02 20:34:15 -0800

    fixed assertEquals for testSquare

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -5,6 +5,6 @@ import org.junit.Test;
 public class SkillDemoTest {
     @Test
     public void testSquare() {
-        assertEquals(4, SkillDemo.square(3)); // Initial test fails 4 does not = 9
+        assertEquals(4, SkillDemo.square(2)); // Test now passes because 4 = 4
     }
 }
